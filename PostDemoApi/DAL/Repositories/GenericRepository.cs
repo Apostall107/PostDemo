f7055311967d3080407bf8e7a2e589007c6ce26b
Oleksandr Hryn <apostalls107@gmail.com>
2023-02-11 03:29:13 +0200

    Generic repo methods now r virtual

--- PostDemoApi/DAL/Repositories/GenericRepository.cs
+++ PostDemoApi/DAL/Repositories/GenericRepository.cs
@@ -8,31 +8,31 @@ namespace PostDemoApi.DAL.Repositories {
         internal DbSet<T> _dbSet;
         protected readonly ILogger _logger;
 
-        public GenericRepository(DatabaseContext context, DbSet<T> dbSet, ILogger logger) {
+        public GenericRepository(DatabaseContext context, ILogger logger) {
             _context = context;
             _logger = logger;
             this._dbSet = _context.Set<T>();
         }
 
-        public async Task<bool> Add(T entity) {
+        public virtual async Task<bool> Add(T entity) {
             await _dbSet.AddAsync(entity);
             return true;
         }
 
-        public async Task<bool> Delete(T entity) {
+        public virtual async Task<bool> Delete(T entity) {
             _dbSet.Remove(entity);
             return true;
         }
 
-        public async Task<T?> GetById(int id) {
+        public virtual async Task<T?> GetById(int id) {
             return await _dbSet.FindAsync(id);
         }
 
-        public async Task<IEnumerable<T>> GetAll() {
-            return  await _dbSet.ToListAsync();
+        public virtual async Task<IEnumerable<T>> GetAll() {
+            return await _dbSet.ToListAsync();
         }
 
-        public async Task<bool> Update(T entity) {
+        public virtual async Task<bool> Update(T entity) {
             _dbSet.Update(entity);
             return true;
         }
