91cbcd6cb2f4f71c9c588ce662153155382a72ac
Oleksandr Hryn <apostalls107@gmail.com>
2023-02-11 03:13:11 +0200

    Gen repo class created

---
+++ PostDemoApi/DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostDemoApi.Contracts;
+
+namespace PostDemoApi.DAL.Repositories {
+    public class GenericRepository<T> : IGenericRepository<T> where T : class {
+
+        protected DatabaseContext _context;
+        internal DbSet<T> _dbSet;
+        protected readonly ILogger _logger;
+
+        public GenericRepository(DatabaseContext context, DbSet<T> dbSet, ILogger logger) {
+            _context = context;
+            _logger = logger;
+            this._dbSet = _context.Set<T>();
+        }
+
+        public Task<bool> Add(T entity) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(T entity) {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(T entity) {
+            throw new NotImplementedException();
+        }
+    }
+}
