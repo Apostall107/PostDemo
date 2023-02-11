06174a326cbfc1c16b3b7486f6c7d78dc999d913
Oleksandr Hryn <apostalls107@gmail.com>
2023-02-11 03:48:32 +0200

    Package repo class created

--- PostDemoApi/Contracts/IPackageRepository.cs
+++ PostDemoApi/Contracts/IPackageRepository.cs
@@ -2,5 +2,6 @@
 
 namespace PostDemoApi.Contracts {
     public interface IPackageRepository : IGenericRepository<Package>{
+        Task<List<Package>?> GetPackagesKilosLess(int kilos);
     }
 }
---
+++ PostDemoApi/DAL/Repositories/PackageRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostDemoApi.Contracts;
+using PostDemoApi.Models;
+
+namespace PostDemoApi.DAL.Repositories {
+    public class PackageRepository : GenericRepository<Package>, IPackageRepository {
+        public PackageRepository(DatabaseContext context, DbSet<Package> dbSet, ILogger logger) : base(context, logger) {
+        }
+
+        public override async Task<IEnumerable<Package>> GetAll() {
+
+            try {
+                return await _context.Packages.Where(x => x.Id < 100).ToListAsync();
+            } catch (Exception e) {
+                Console.Write(e);
+                throw;
+            }
+
+        }
+
+        public async Task<List<Package>?> GetPackagesKilosLess(int kilos) {
+
+            try {
+                return await _context.Packages.Where(x => x.Kilos < kilos).ToListAsync();
+            } catch (Exception e) {
+                Console.Write(e);
+                throw;
+            }
+        }
+    }
+}
