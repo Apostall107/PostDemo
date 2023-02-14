e0b73ba244d853b00fa910864f24460c596104e0
Oleksandr Hryn <apostalls107@gmail.com>
2023-02-14 03:26:40 +0200

    Cleaned up UnitOfWork

--- PostDemoApi/DAL/UnitOfWork.cs
+++ PostDemoApi/DAL/UnitOfWork.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PostDemoApi.Contracts;
 using PostDemoApi.DAL.Repositories;
-using PostDemoApi.DAL;
 
 namespace PostDemoApi.DAL {
     public class UnitOfWork : IUnitOfWork, IDisposable {
@@ -31,64 +30,4 @@ namespace PostDemoApi.DAL {
             _context.Dispose();
         }
     }
-
-
-public static class ClientEndpoints
-{
-	public static void MapClientEndpoints (this IEndpointRouteBuilder routes)
-    {
-        routes.MapGet("/api/Client", async (DatabaseContext db) =>
-        {
-            return await db.Clients.ToListAsync();
-        })
-        .WithName("GetAllClients");
-
-        routes.MapGet("/api/Client/{id}", async (int Id, DatabaseContext db) =>
-        {
-            return await db.Clients.FindAsync(Id)
-                is Client model
-                    ? Results.Ok(model)
-                    : Results.NotFound();
-        })
-        .WithName("GetClientById");
-
-        routes.MapPut("/api/Client/{id}", async (int Id, Client client, DatabaseContext db) =>
-        {
-            var foundModel = await db.Clients.FindAsync(Id);
-
-            if (foundModel is null)
-            {
-                return Results.NotFound();
-            }
-            
-            db.Update(client);
-
-            await db.SaveChangesAsync();
-
-            return Results.NoContent();
-        })   
-        .WithName("UpdateClient");
-
-        routes.MapPost("/api/Client/", async (Client client, DatabaseContext db) =>
-        {
-            db.Clients.Add(client);
-            await db.SaveChangesAsync();
-            return Results.Created($"/Clients/{client.Id}", client);
-        })
-        .WithName("CreateClient");
-
-
-        routes.MapDelete("/api/Client/{id}", async (int Id, DatabaseContext db) =>
-        {
-            if (await db.Clients.FindAsync(Id) is Client client)
-            {
-                db.Clients.Remove(client);
-                await db.SaveChangesAsync();
-                return Results.Ok(client);
-            }
-
-            return Results.NotFound();
-        })
-        .WithName("DeleteClient");
-    }
-}}
+}
