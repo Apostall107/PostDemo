b6f932e5b34b45ddf21fd79e2a3392617f4ae4ca
Yevhen Vasylynchuk <marakasi88887@gmail.com>
2023-03-02 23:13:22 +0200

    Implemented Email smtp client

---
+++ PostDemo.BL/Helpers/SMTP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostDemo.BL.Helpers
+{
+    public class SMTP : IDisposable
+    {
+        private bool isDisposed;
+
+        public void Dispose()
+        {
+            isDisposed = true;
+        }
+
+        public void Send()
+        {
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ PostDemo.BL/Helpers/SMTPConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostDemo.BL.Helpers
+{
+    public class SMTPConfig
+    {
+        private bool _isConfigured;
+        public bool IsConfigured
+        {
+            get { return _isConfigured; }
+            set { _isConfigured = value; }
+        }
+    }
+}
